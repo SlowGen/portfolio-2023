aaf9d74de915786f78143c1e18dd316fe4243c5c
slowgen <kali@kali.nyc>
2023-03-01 14:15:00 -0500

    added header and content structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Box } from '@mui/material';
 import { styles } from './App.styles';
+import { Content } from './components/Content';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <Box sx={styles.app}>
-      page
+      <Header />
+      <Content />
     </Box>
   )
 }
