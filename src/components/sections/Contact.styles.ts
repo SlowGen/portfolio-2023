208cb88a7dc66cf6bc17cc7856e5292026af31e6
slowgen <kali@kali.nyc>
2023-03-06 13:57:10 -0500

    contact

---
+++ src/components/sections/Contact.styles.ts
@@ -0,0 +1,36 @@
+export const styles = {
+  contactContainer: {
+    marginLeft: '75px',
+    marginRight: '75px',
+    padding: '10px',
+    backgroundColor: 'rgba(171, 162, 157, 0.5)',
+    borderRadius: '20px',
+  },
+  contactContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  card: {
+    minHeight: '150px',
+    width: '400px',
+  },
+  text: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+  },
+  icon: {
+    fontSize: '50px',
+    padding: '30px',
+    color: '#635b57',
+  },
+  nav: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}
--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,5 +1,46 @@
+import { Box, Button, Link,Typography } from "@mui/material";
 import { ContentProps } from "../../App"
+import { SectionCard } from "../SectionCard";
+import { styles } from "./Contact.styles";
+import { CodeOutlined, DescriptionOutlined, MailOutline } from "@mui/icons-material";
 
 export const Contact = (props: ContentProps) => {
-  return <div>contact</div>
+  const { display } = props;
+
+  return (
+    <Box sx={styles.contactContainer}>
+      <Typography variant="h5">
+        Get in touch!
+      </Typography>
+      <Box sx={styles.contactContent}>
+        <Link href="mailto:info@kali.nyc" color='inherit'
+          target='_blank' rel='noopener'>
+          <MailOutline sx={styles.icon}/>
+        </Link>
+        <Link href="https://github.com/slowgen" color='inherit'
+          target='_blank' rel='noopener'>
+          <CodeOutlined sx={styles.icon}/>
+        </Link>
+        <Link href="https://www.linkedin.com/in/kali-nfn/" color='inherit'
+          target='_blank' rel='noopener'>
+          <DescriptionOutlined sx={styles.icon}/>
+        </Link>
+      </Box>
+      <Box sx={styles.nav}>
+        <Button onClick={() => display('main')}>
+          <SectionCard section="main"/>
+        </Button>
+        <Button onClick={() => display('about')}>
+          <SectionCard section="about"/>
+        </Button>
+        <Button onClick={() => display('projects')}>
+          <SectionCard section="projects"/>
+        </Button>
+        <Button onClick={() => display('gallery')}>
+          <SectionCard section="gallery"/>
+        </Button>
+      </Box>
+      <iframe name="popup" title="SlowGen" height={0} width={0} />
+    </Box>
+  )
 }
