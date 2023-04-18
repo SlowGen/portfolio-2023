0c8e17d55cbdb87b20862b4a7f304d6a4a5a8c3b
slowgen <kali@kali.nyc>
2023-04-18 17:23:10 -0400

    update bio

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -13,13 +13,16 @@ export const About = (props: ContentProps) => {
       <Box sx={styles.aboutContent}>
         <Box sx={styles.text}>
           <Typography variant='body2'>
-            Raised in Oregon, I started my career in tech as the webmaster for local band The Dandy Warhols. After moving to NYC, I pursued a career in hospitality but found myself drawn back to my love of technology.
+            Originally from Oregon, I have always been passionate about technology. My journey began when I landed my first job as a webmaster for the popular local band, The Dandy Warhols. I then moved to New York City and embarked on a career in hospitality. My heart belonged to the tech world and it was only a matter of time for me to find my way back.
           </Typography>
           <Typography variant='body2'>
-            In 2019, I left my successful past behind and jumped head-first into coding. After weeks of teaching myself Python and data visualization, I decided going the bootcamp route was right for me. In 2020 I graduated from the Grace Hopper program at Fullstack Academy where I learned JavaScript and the NERD stack.
+            In 2019, I made the bold decision to leave my successful past behind and immerse myself in the world of coding. I spent countless hours teaching myself Python and data visualization before ultimately deciding to pursue a bootcamp program. In 2020, I graduated from the Grace Hopper program at Fullstack Academy, where I honed my skills in JavaScript, React, Express and Postgres.
           </Typography>
           <Typography variant="body2">
-            Since then, I've learned several new frameworks, languages and technologies. Spent a couple years working on video capture and playback, and played around with some fun side projects.
+            Since then, I have continued to expand my knowledge and expertise, diving into new frameworks, languages, and technologies. I spent two years working on video capture and playback, and have also enjoyed tinkering with several exciting side projects.
+          </Typography>
+          <Typography variant="body2">
+            Overall, my journey has been one of dedication, hard work, and a relentless pursuit of knowledge. I am thrilled to be a part of the ever-evolving tech industry and am excited to see where my skills and passion take me next.
           </Typography>
         </Box>
         <Box sx={styles.img}>
